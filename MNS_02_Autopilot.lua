6087e37bfd1bb22cb5b15fbab3a5948841b77ee4
Carl August Gjørsvik <Carl.Gjorsvik@student.uib.no>
2020-08-16 20:24:04 +0200

    Changed autopilot interface to use a button to add waypoint at center. Changed design of screen buttons to reflect clicking and activation

--- MNS_02_Autopilot.lua
+++ MNS_02_Autopilot.lua
@@ -7,7 +7,7 @@ arr={}
 io={}
 io.gN,io.gB,io.sN,io.sB=input.getNumber,input.getBool,output.setNumber,output.setBool
 s={}
-s.drf,s.dt,s.sc,s.smco,s.smcs,s.smcn=screen.drawRectF,screen.drawText,screen.setColor,screen.setMapColorOcean,screen.setMapColorShallows,screen.setMapColorSnow
+s.drf,s.dt,s.sc,s.dtb,s.dr=screen.drawRectF,screen.drawText,screen.setColor,screen.drawTextBox,screen.drawRect
 m={ms=map.mapToScreen}
 
 function onTick()
@@ -15,40 +15,11 @@ function onTick()
   on = io.gB(3)
   
 	if ini and on then
-    gX=io.gN(7)
-    gY=io.gN(8)
-    sp=io.gN(10)
-    head=io.gN(11)
-    mT=io.gN(12)
-    acc=io.gN(13)
-    dec=io.gN(14)
-    ruD=io.gN(15)
-    dX=io.gN(16)
-    dY=io.gN(17)
-    cX=io.gN(18)
-    cY=io.gN(19)
-    zoom=io.gN(20)
+    gX=io.gN(7);gY=io.gN(8);sp=io.gN(10);head=io.gN(11);mT=io.gN(12);acc=io.gN(13);dec=io.gN(14);ruD=io.gN(15);dX=io.gN(16);dY=io.gN(17);cX=io.gN(18);cY=io.gN(19);zoom=io.gN(20)
     
-    ti=ti+1
-    if ti>600 then ti=100 end
-    if ti>1 then hl=0 end
-    
-    if io.gB(1) then
-      tX = io.gN(3)
-      tY = io.gN(4)
-      if tC(w-23,0,w-10,8) and hl>30 then AP=not AP;ru=0;ttl=0;hl=0
-      elseif tC(w-11,0,w-3,8) and hl>50 then nWP=0;hl=0;stop()
-      elseif hl>40 and (tC(6,6,w-6,h-7) or tC(0,6,w,(h/2)-4)) then
-        wPb=true;nWP=math.min(nWP+1,8)
-        if nWP>0 and nWP<9 then arr[nWP].x,arr[nWP].y=map.screenToMap(cX,cY,zoom,w,h,tX,tY) end
-        hl=0
-      end
-      hl=hl+1
-      ti=0
-    end
-    
-    headInputToDegrees(head)
-    
+    cl = io.gB(1);tX=io.gN(3);tY=io.gN(4);clk(tX,tY,cl)
+    if cl then hl=hl+1 else hl=0 end
+    toDeg(head)
     if wPb then
       t=bD(arr[1].x,arr[1].y)
       if t.d<50 and ti>30 then
@@ -56,37 +27,54 @@ function onTick()
         for i=1,7 do
           arr[i].x=arr[i+1].x;arr[i].y=arr[i+1].y
         end
-        if nWP==0 then stop() end
-        ti=0
+        if nWP==0 then stop() end;ti=0
       end
     end
     
     if AP then
-      setRu()
-      setTh()
+      setRu();setTh()
     end
-    io.sB(1,AP)
-    io.sN(1,ru)
-    io.sN(2,ttl)
+    io.sB(1,AP);io.sN(1,ru);io.sN(2,ttl)
   end
 end
 
-function headInputToDegrees(head)
-  if head < 0 then
-    headD = (math.abs(head))*360
-  elseif head == 0 then
-    headD = 0
-  else
-    headD = 360-head*360
-  end
+function clk(x, y,c)
+	for k,b in pairs(buttons) do
+		if inrange(x,y,b.x,b.y,b.w,b.h) and c then
+      if b.hold and hl>=b.ti and not b.a then
+        b.f(b)
+        b.a=true
+      elseif not b.hold then
+        b.f(b)
+      end
+      b.p=true
+    else
+      b.p=false;b.a=false
+		end
+	end
+end
+
+function addBtn(x,y,w,h,t,f,ti,hold,r)
+	data = {
+		['x']=(x or 0),['y']=(y or 0),
+		['w']=(w or 0),['h']=(h or 0),
+		['t']=(t or "err"),['f']=(f or nil),
+    ['r']=(r or false),['ti']=(ti or 0),
+    ['hold']=(hold or false),['a']=false,
+    ['p']=false
+	}
+	table.insert(buttons,data)
+end
+
+function toDeg(head)
+  if head < 0 then headD = (math.abs(head))*360
+  elseif head == 0 then headD = 0
+  else headD = 360-head*360 end
 end
 
 function setRu()
-  diffL=((headD-t.b+360)%360)
-  diffR=((t.b-headD+360)%360)
-  diff=math.min(diffL,diffR)
-  if diffL<diffR then ru=ru-ruD
-  else ru=ru+ruD end
+  diffL=((headD-t.b+360)%360);diffR=((t.b-headD+360)%360);diff=math.min(diffL,diffR)
+  if diffL<diffR then ru=ru-ruD else ru=ru+ruD end
   if ru<0 and ru<-diff/30 then ru=math.max(-diff/30,-1) end
   if ru>0 and ru>diff/30 then ru=math.min(diff/30,1) end
   if ru<-1 then ru=-1 elseif ru>1 then ru=1 end
@@ -101,44 +89,50 @@ function setTh()
   ttlT=math.min(tuTh, dTh)
   if ttl<ttlT then ttl=ttl+acc elseif ttl>ttlT then ttl=ttl-dec end
 end
-
 function stop()
-  AP=false;wPb=false
-  ttl=0;ru=0
+  AP=false;wPb=false;ttl=0;ru=0
+end
+function init()
+	ini=true;c={x=w/2,y=h/2};f=h/32
+  for i=1,8 do arr[i]={x=0,y=0} end
+  buttons={}
+  addBtn(w-18,h-7,18,6,"WP",function() addWP() end,0,true)
+  addBtn(w-23,0,13,6,"AP",function() AP=not AP;ru=0;ttl=0;hl=0 end,30,true)
+  addBtn(w-11,0,8,6,"X",function() nWP=0;hl=0;stop() end,50,true)
 end
 
-function init()
-	ini=true
-  c={x=w/2,y=h/2}
-  f=h/32
-  for i=1,8 do
-    arr[i]={x=0,y=0}
-  end
+function addWP()
+  wPb=true;nWP=math.min(nWP+1,8)
+  arr[nWP].x,arr[nWP].y=map.screenToMap(cX,cY,zoom,w,h,c.x,c.y)
+end
+
+function inrange(cx, cy, x, y, w, h)
+	return (cx>=x and cy>=y and cx<=(x+w) and cy<=(y+h))
 end
 
 function onDraw()
-  w = screen.getWidth()
-  h = screen.getHeight()
+  w=screen.getWidth();h=screen.getHeight()
   if not ini then init() end
-  
   if on then
-  
-    s.sc(150,0,0,150)
-    s.dt(w-10,f-1,"X")
-    if AP then
-    s.drf(w-23,0,11,7)
-    s.sc(0,0,0)
+    for k,b in pairs(buttons) do
+      if b.p then
+        s.sc(255,255,255);s.drf(b.x,b.y,b.w,b.h)
+        s.dr(b.x,b.y,b.w,b.h);s.sc(0,0,0)
+        s.dtb(b.x+1,b.y+1,b.w,b.h,b.t,0,0)
+      else
+        s.sc(150,0,0,150)
+        if b.r then s.dr(b.x,b.y,b.w,b.h) end
+        s.dtb(b.x+1,b.y+1,b.w,b.h,b.t,0,0)
+      end
     end
-    s.dt(w-22,f-1,"AP")
     
+    if AP then s.sc(0,100,0,150);s.dr(w-23,0,13,6) end
 
     if wPb then
       for i=1,nWP do
         tarX,tarY=m.ms(cX,cY,zoom,w,h,arr[i].x,arr[i].y)
-        s.sc(100,0,0,100)
-        s.drf(tarX-2,tarY-2,6,7)
-        s.sc(0,0,0,100)
-        s.dt(tarX-1,tarY-1,i)
+        s.sc(100,0,0,100);s.drf(tarX-2,tarY-2,6,7)
+        s.sc(0,0,0,100);s.dt(tarX-1,tarY-1,i)
       end
     end
     
@@ -155,8 +149,4 @@ function bD(x,y)
   t.b=180*(math.atan(x-gX, y-gY))/math.pi
   if t.b<0 then t.b=360+t.b end
   return t
-end
-
-function tC(x1,y1,x2,y2)
-  return (tX>x1 and tX<x2 and tY>y1 and tY<y2)
 end
